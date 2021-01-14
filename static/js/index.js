ee19287e450b38c01997155063182dad991f9ea3
unknown <>
2021-01-14 16:31:06 -0600

    Initial Commit

---
+++ static/js/index.js
@@ -0,0 +1,37 @@
+/*mobile menu code*/
+
+var hamburger = document.getElementById("hamburger");
+var mobileMenu = document.getElementById("mobile-menu");
+var line1 = document.getElementById("line1");
+var line2 = document.getElementById("line2");
+var line3 = document.getElementById("line3");
+
+hamburger.addEventListener("click", function(){
+    if (mobileMenu.style.display === "block"){
+      mobileMenu.style.display = "none";
+      line1.classList.remove('line1clicked');
+      line2.classList.remove('line2clicked');
+      line3.classList.remove('line3clicked');
+    } else{
+      mobileMenu.style.display = "block";
+      line1.classList.add('line1clicked');
+      line2.classList.add('line2clicked');
+      line3.classList.add('line3clicked');
+    }
+});
+/*make the menu close after clicking menu item*/
+var menuItem = document.getElementsByClassName("menu-item");
+
+for (var i = 0; i < menuItem.length; i++) {
+	menuItem[i].addEventListener("click", function(){
+		if (mobileMenu.style.display === "block"){
+			mobileMenu.style.display = "none";
+      line1.classList.remove("line1clicked");
+      line2.classList.remove("line2clicked");
+      line3.classList.remove("line3clicked");
+		} else {
+			mobileMenu.style.display = "block";
+		}
+	});
+
+}
